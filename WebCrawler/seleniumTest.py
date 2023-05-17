19a04e0caae581d3e1452ebd6c6ea1c45e4a3a4d
kingjay <bill_satya@outlook.com>
2023-05-17 16:20:46 +0800

    下载chitChat(chatgpt套壳)保存的问答记录

--- WebCrawler/seleniumTest.py
+++ WebCrawler/seleniumTest.py
@@ -3,7 +3,7 @@
 # Author:king
 # Date:2023/5/17
 
-
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -13,3 +13,4 @@ chrome.get("https://www.baidu.com")
 # chrome.implicitly_wait(10)  # 隐式等待 10 秒钟
 element = chrome.find_element(By.ID, 'kw')
 element.send_keys('test')
+time.sleep(2)
