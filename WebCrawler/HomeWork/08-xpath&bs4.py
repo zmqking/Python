d3dd15f7d54084a1d5cbf2de175f1307a79f4c40
zmqking12 <1148092466@qq.com>
2023-06-24 22:04:58 +0800

    添加bs4数据提取

--- WebCrawler/HomeWork/08-xpath&bs4.py
+++ WebCrawler/HomeWork/08-xpath&bs4.py
@@ -5,44 +5,54 @@
 @Author  : king
 @Desc    : 
 """
+# region xpath解析
+# import requests
+# from lxml import etree
+#
+# class LianJia():
+#
+#     def __init__(self):
+#         self.head = {
+#             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+#         }
+#         self.url = 'https://sh.lianjia.com/ershoufang/pudong/pg2/'
+#         self.titleXpath = '//li//div[@class="title"]/a'
+#         self.positionXpath = '//div[@class="positionInfo"]/a'
+#         self.layoutXpath = '//div[@class="houseInfo"]/text()'
+#         self.fllowXpath = '//li//div[@class="followInfo"]/text()'
+#         self.priceXpath = '//div[@class="priceInfo"]/div/span/text()'
+#         self.amountXpath = '//li//div[@class="unitPrice"]/span/text()'
+#
+#     def getContent(self):
+#         response = requests.get(self.url, self.head)
+#         return response.text
+#
+#     def analysis(self):
+#         txt = self.getContent()
+#         tree = etree.HTML(txt)
+#         title_list = tree.xpath(self.titleXpath)
+#         positionXpath = tree.xpath(self.positionXpath)
+#         layoutXpath = tree.xpath(self.layoutXpath)
+#         fllowXpath = tree.xpath(self.fllowXpath)
+#         priceXpath = tree.xpath(self.priceXpath)
+#         amountXpath = tree.xpath(self.amountXpath)
+#         for i in range(len(title_list)):
+#             print(title_list[i].text)
+#             print(positionXpath[i].text)
+#             print(layoutXpath[i])
+#             print(fllowXpath[i])
+#             print(priceXpath[i])
+#             print(amountXpath[i])
+#             print('----------------------------')
+#
+# if __name__ == '__main__':
+#     lianjia = LianJia()
+#     lianjia.analysis()
+# endregion
+
+from bs4 import BeautifulSoup
 import requests
-from lxml import etree
 
-class LianJia():
-
-    def __init__(self):
-        self.head = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-        }
-        self.url = 'https://sh.lianjia.com/ershoufang/pudong/pg2/'
-        self.titleXpath = '//li//div[@class="title"]/a'
-        self.positionXpath = '//div[@class="positionInfo"]/a'
-        self.layoutXpath = '//div[@class="houseInfo"]/text()'
-        self.fllowXpath = '//li//div[@class="followInfo"]/text()'
-        self.priceXpath = '//div[@class="priceInfo"]/div/span/text()'
-        self.amountXpath = '//li//div[@class="unitPrice"]/span/text()'
-
-    def getContent(self):
-        response = requests.get(self.url, self.head)
-        return response.text
-
-    def analysis(self):
-        txt = self.getContent()
-        tree = etree.HTML(txt)
-        title_list = tree.xpath(self.titleXpath)
-        positionXpath = tree.xpath(self.positionXpath)
-        layoutXpath = tree.xpath(self.layoutXpath)
-        fllowXpath = tree.xpath(self.fllowXpath)
-        priceXpath = tree.xpath(self.priceXpath)
-        amountXpath = tree.xpath(self.amountXpath)
-        for i in range(len(title_list)):
-            print(title_list[i].text)
-            print(positionXpath[i].text)
-            print(layoutXpath[i])
-            print(fllowXpath[i])
-            print(priceXpath[i])
-            print(amountXpath[i])
-            print('----------------------------')
 
 class LianJia_bs4():
     def __init__(self):
@@ -51,7 +61,21 @@ class LianJia_bs4():
         }
         self.url = 'http://ip.yqie.com/ipproxy.htm'
 
+    def GetContent(self):
+        response = requests.get(self.url, headers=self.head)
+        # 怎么判断当前用什么方式解析？
+        # 从网页head 里面查找编码
+        soup = BeautifulSoup(response.content.decode('utf-8'), 'lxml')
+        # el_list = soup.find_all('span') #元素查找
+        # el_list = soup.find_all(attrs={'class': 'divcenter'})  # 属性查找
+        el_list = soup.select('#GridViewOrder tr:not(:first-child)')
+        for item in el_list:
+            tds = item.select('td')
+            print(
+                tds[0].text + ',' + tds[1].text + ',' + tds[2].text + ',' + tds[3].text + ',' + tds[4].text + ',' + tds[
+                    5].text)
+
 
 if __name__ == '__main__':
-    lianjia = LianJia()
-    lianjia.analysis()
+    lianjia = LianJia_bs4()
+    lianjia.GetContent()
--- WebCrawler/seleniumTest.py
+++ WebCrawler/seleniumTest.py
@@ -8,9 +8,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-chrome = webdriver.Chrome()
-chrome.get("https://www.baidu.com")
-# chrome.implicitly_wait(10)  # 隐式等待 10 秒钟
-element = chrome.find_element(By.ID, 'kw')
+# region 不自动关闭浏览器
+# 获取配置对象 => 什么样的浏览器就选择什么浏览器配置
+option = webdriver.ChromeOptions()
+option.add_experimental_option("detach", True)
+
+# 获取driver对象, 并将配置好的option传入进去
+driver = webdriver.Chrome(options=option)
+driver.get('https://www.baidu.com')
+element = driver.find_element(By.ID, 'kw')
 element.send_keys('test')
-time.sleep(2)
+# endregion
+
+
+# region 自动关闭浏览器
+# chrome = webdriver.Chrome()
+# chrome.get("https://www.baidu.com")
+# # chrome.implicitly_wait(10)  # 隐式等待 10 秒钟
+# element = chrome.find_element(By.ID, 'kw')
+# element.send_keys('test')
+# time.sleep(2)
+# endregion
+
+
