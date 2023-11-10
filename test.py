12dc50e6aa00d4d00d598ca68e22998c0422cd73
bill_satya <bill_satya@outlook.com>
2023-11-10 10:21:02 +0800

    增加自动学习八大员课程（seleniumTest）

--- test.py
+++ test.py
@@ -9,29 +9,66 @@
 
 # print(execjs.eval('Date.now()'))
 
-arrayA = [1, 3, 87, 28, 90]
-arrayB = [1, 96, 87, 8, 90]
+import asyncio
+import time
 
-def getTextToC():
-    return 'from python';
+async def fetch_data(url):
+    # 模拟异步网络请求，这里使用 asyncio.sleep 来代替实际的网络请求
+    await asyncio.sleep(1)
+    return f"Data fetched from {url}"
 
-def findSameOrDiff():
-    diff = []
-    same = []
-    for a in arrayA:
-        for b in arrayB:
-            if a == b:
-                same.append(a)
+async def process_data(data):
+    # 模拟异步数据处理
+    await asyncio.sleep(2)
+    return f"Processed data: {data.upper()}"
 
-    arrayA.extend(arrayB)
-    for i in arrayA:
-        if same.count(i) == 0:
-            diff.append(i)
-    print(f'相同:{same}')
-    print(f'不同:{diff}')
+async def main():
+    # 启动异步任务并等待结果
+    start = time.time()
+    url = "https://example.com"
+    task1 = asyncio.create_task(fetch_data(url))
+    task2 = asyncio.create_task(process_data(await task1))
+
+    # 并发执行任务
+    await asyncio.gather(task1, task2)
+
+    # 获取结果
+    fetched_data = task1.result()
+    processed_data = task2.result()
+
+    # 打印结果
+    print(fetched_data)
+    print(processed_data)
+    print(f"用时：{time.time()-start}")
+# 运行主函数
+asyncio.run(main())
 
 
-findSameOrDiff()
+
+# 找出相同的值
+# arrayA = [1, 3, 87, 28, 90]
+# arrayB = [1, 96, 87, 8, 90]
+#
+# def getTextToC():
+#     return 'from python';
+#
+# def findSameOrDiff():
+#     diff = []
+#     same = []
+#     for a in arrayA:
+#         for b in arrayB:
+#             if a == b:
+#                 same.append(a)
+#
+#     arrayA.extend(arrayB)
+#     for i in arrayA:
+#         if same.count(i) == 0:
+#             diff.append(i)
+#     print(f'相同:{same}')
+#     print(f'不同:{diff}')
+#
+#
+# findSameOrDiff()
 
 # def login(func):
 #     def wrapper(*args, **kwargs):
