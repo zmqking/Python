3bc5ef06a15ac19e5d6eccb991a71f266b9b001c
zmqking <hnzhangmeiqing@163.com>
2023-11-25 18:41:14 +0800

    增加获取某个目录视频列表的长度

---
+++ Temp/ReadVideoInfo.py
@@ -0,0 +1,37 @@
+import cv2
+import os
+
+def get_all_files(directory):
+    all_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            all_files.append(os.path.join(root, file))
+    return all_files
+
+
+# 判断是否为视频文件
+def is_video_file(file_path):
+    video_extends = ['.mp4']
+    return any(file_path.lower().endswith(ext) for ext in video_extends)
+
+# 获取视频长度
+def GetVideoSeconds(video_path):
+    global seconds
+    cap = cv2.VideoCapture(video_path)
+    length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    seconds = int(length / fps)
+
+path = input('请输入目录地址：')
+while not any(path):
+    path = input('请输入目录地址：')
+
+all_files = get_all_files(path)
+# 遍历获取的视频列表
+video_files = [file for file in all_files if is_video_file(file)]
+
+print('视频列表')
+for video_path in video_files:
+    print(video_path)
+    GetVideoSeconds(video_path)
+    print('视频长度:%s'%seconds)
