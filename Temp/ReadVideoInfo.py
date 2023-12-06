729abf116b64c39904654ce182f6c25b395ac123
bill_satya <bill_satya@outlook.com>
2023-12-06 16:45:17 +0800

    增加文件类型判断

--- Temp/ReadVideoInfo.py
+++ Temp/ReadVideoInfo.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 
+
 def get_all_files(directory):
     all_files = []
     for root, dirs, files in os.walk(directory):
@@ -11,16 +12,18 @@ def get_all_files(directory):
 
 # 判断是否为视频文件
 def is_video_file(file_path):
-    video_extends = ['.mp4']
+    video_extends = ['.mp4', '.ts']
     return any(file_path.lower().endswith(ext) for ext in video_extends)
 
+
 # 获取视频长度
-def GetVideoSeconds(video_path):
+def Get_Video_Seconds(video_path):
     global seconds
     cap = cv2.VideoCapture(video_path)
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     fps = cap.get(cv2.CAP_PROP_FPS)
     seconds = int(length / fps)
+
 
 path = input('请输入目录地址：')
 while not any(path):
@@ -30,8 +33,22 @@ all_files = get_all_files(path)
 # 遍历获取的视频列表
 video_files = [file for file in all_files if is_video_file(file)]
 
-print('视频列表')
+print('视频列表:%s' % len(video_files))
 for video_path in video_files:
-    print(video_path)
-    GetVideoSeconds(video_path)
-    print('视频长度:%s'%seconds)
+    video_name = os.path.basename(video_path)
+    Get_Video_Seconds(video_path)
+    sql = "update T_XueXi_ZiYuan set Duration={} where SName='{}' and ZType=1;"
+    print(sql.format(seconds, video_name))
+    print('''
+        IF @@ROWCOUNT = 0
+        BEGIN
+            PRINT '%s'
+        END
+        ELSE
+        BEGIN
+            IF @@ROWCOUNT > 1
+            BEGIN
+                PRINT '%s'
+            END    
+        END
+    ''' %(video_name,video_name))
