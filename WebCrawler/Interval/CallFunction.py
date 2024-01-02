17366153786ef8ac94c863112c0dd61d57c38a8f
bill_satya <bill_satya@outlook.com>
2024-01-02 14:02:11 +0800

    跨文件调用函数

--- WebCrawler/Interval/CallFunction.py
+++ WebCrawler/Interval/CallFunction.py
@@ -1,26 +1,50 @@
 # -*- coding: UTF-8 -*-
-# Project -> File:Python -> main 
+# Project -> File:Python -> main
 # Author:king
 # Date:12/21/2023
-import os
-import urllib.parse
-import urllib.request
 
-def sc_send(text, desp='', key='[SENDKEY]'):
-    postdata = urllib.parse.urlencode({'text': text, 'desp': desp}).encode('utf-8')
-    url = f'https://sctapi.ftqq.com/{key}.send'
-    req = urllib.request.Request(url, data=postdata, method='POST')
-    with urllib.request.urlopen(req) as response:
-        result = response.read().decode('utf-8')
-    return result
+# scheduler.py
 
-data = {}
-with open(os.path.join(os.path.dirname(__file__), '.env'), 'r') as f:
-    for line in f:
-        key, value = line.strip().split('=')
-        data[key] = value
-key = data['SENDKEY']
+from redis import Redis
+from rq_scheduler import Scheduler
+from Function import test
+# from datetime import datetime, timedelta
 
-ret = sc_send('测试提示信息', '第一行\n\n第二行', key)
-print(ret)
+# Connect to Redis server
+redis_conn = Redis(host='localhost', port=6379, db=0)
 
+# Create a scheduler
+scheduler = Scheduler(connection=redis_conn)
+
+# Schedule the job to run every 10 seconds
+scheduler.cron(
+    cron_string='*/10 * * * *',  # Run every 10 seconds
+    func=test,
+    queue_name='default'
+)
+
+# Start the scheduler
+scheduler.run()
+
+
+# region 定时调用
+# from Function import test
+# import threading
+# import time
+#
+# def my_function():
+#     print("Timer expired! This function is called.")
+#
+# # 创建一个定时器，每5秒调用一次my_function
+# timer = threading.Timer(5, my_function)
+#
+# # 启动定时器
+# timer.start()
+#
+# # 主线程可以继续执行其他操作
+#
+# # 等待定时器完成
+# timer.join()
+#
+# print("Main thread continues to run...")
+# endregion
---
+++ WebCrawler/Interval/Function.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+# Project -> File:Python -> Function.py 
+# Author:king
+# Date:12/25/2023
+
+
+def test():
+    print("hello world")
