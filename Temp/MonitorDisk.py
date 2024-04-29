1e6d5ea79477708c3bb5d0fda895684a31cc2d55
bill_satya <bill_satya@outlook.com>
2024-04-29 13:47:13 +0800

    发送邮件磁盘增加空间显示

--- Temp/MonitorDisk.py
+++ Temp/MonitorDisk.py
@@ -12,6 +12,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import atexit
 
+
 class Monitor():
     sender_email = "276324922@qq.com"  # Modify to your email address
     sender_password = "yvqkogpmqjssbgjf"  # Modify to your email password
@@ -21,7 +22,7 @@ class Monitor():
     def check_disk_space(self, path, min_space):
         disk_usage = psutil.disk_usage(path)
         free_space = disk_usage.free / (1024 ** 3)  # Convert bytes to gigabytes
-        return free_space < min_space
+        return free_space < min_space, free_space
 
     def del_files(self, file_path):
         try:
@@ -64,19 +65,22 @@ class Monitor():
     def job(self):
         # del_directory = "E:\Chrome\gate"
         del_directory = "D:\DBBase\MSSQL10.MSSQL2008\MSSQL\Backup\ZL_TrainingPlatform"
-        # del_directory = "D:\DBBase\MSSQL10.MSSQL2008\MSSQL\Backup\ZL_TrainingPlatform_DM"
-
-        oldest_file = self.get_oldest_file(del_directory)
-        if len(oldest_file) > 0:
-            self.del_files(oldest_file)
-        else:
-            print('Number of database files less than 5')
-            self.send_email('168 server Number of database backup files less than 5')
 
         disk_path = "D:\\"  # Modify to your D drive path
         min_disk_space = 5  # Minimum required space in GB
 
-        if self.check_disk_space(disk_path, min_disk_space):
+        oldest_file = self.get_oldest_file(del_directory)
+        if len(oldest_file) > 0:
+            self.del_files(oldest_file)
+            _, free_space = self.check_disk_space(disk_path, min_disk_space)
+            self.send_email(
+                f'disk space remaining: {round(free_space, 2)}G \r\n deleted dabase backup files:{oldest_file}')
+        else:
+            print('Number of database files less than 5')
+            self.send_email('168 server Number of database backup files less than 5')
+
+        flag, _ = self.check_disk_space(disk_path, min_disk_space)
+        if flag:
             self.send_email('168 server Disk space is less than 5GB. Please free up space immediately.')
             print("Disk space is less than 5GB. send mail notice...")
             # self.del_files(disk_path)
@@ -86,7 +90,7 @@ class Monitor():
 
 if __name__ == '__main__':
     monitor = Monitor()
-
+    # monitor.job()
     # Schedule the job to run every day at 3:00 AM
     schedule.every().day.at("03:00").do(monitor.job)
     print('start application...')
